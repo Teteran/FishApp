6286336e47023222a9082fc10955bbde1f984448
Kacper Tetera <kte@astec.net>
2021-02-20 21:51:04 +0100

    clear code

--- App/Sagas/index.js
+++ App/Sagas/index.js
@@ -1,8 +1,6 @@
 import { takeLatest, all } from 'redux-saga/effects'
-import { ExampleTypes } from 'App/Stores/Example/Actions'
 import { WeatherTypes } from 'App/Stores/Weather/Actions'
 import { StartupTypes } from 'App/Stores/Startup/Actions'
-import { fetchUser } from './ExampleSaga'
 import { fetchWeatherData } from './WeatherSaga'
 import { startup } from './StartupSaga'
 
@@ -13,8 +11,6 @@ export default function* root() {
      */
     // Run the startup saga when the application starts
     takeLatest(StartupTypes.STARTUP, startup),
-    // Call `fetchUser()` when a `FETCH_USER` action is triggered
-    takeLatest(ExampleTypes.FETCH_USER, fetchUser),
     takeLatest(WeatherTypes.FETCH_WEATHER_DATA, fetchWeatherData),
   ])
 }
